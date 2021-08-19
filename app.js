38b5f958ea554cacf85800e866e3a0913401df33
Annelies-P <annelies.pauwels@gmail.com>
2021-08-19 16:57:46 +0200

    make processing async

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import { app, errorHandler } from 'mu';
 import { getNotStartedPublicationTasks, TASK_ONGOING_STATUS } from './lib/publication-task';
 
-
 // todo configure delta notifier to send deltas for publication tasks having adms:status not started
 
 app.post('/delta', async function (req, res, next) {
@@ -11,8 +10,10 @@ app.post('/delta', async function (req, res, next) {
 
   if (publicationTasks) {
     console.log(`Found ${publicationTasks.length} publication tasks to be processed.`);
-    console.log(publicationTasks);
-    publicationTasks.forEach( publicationTask => publicationTask.process());
+    for (const publicationTask of publicationTasks) {
+      await publicationTask.persistStatus(TASK_ONGOING_STATUS);
+      publicationTask.process(); // asynchronous: we're not waiting for the result
+    };
     return res.status(202).end();
   } else {
     console.log(`No publication tasks found to be processed.`);
@@ -20,6 +21,5 @@ app.post('/delta', async function (req, res, next) {
   }
 
 });
-
 
 app.use(errorHandler);
