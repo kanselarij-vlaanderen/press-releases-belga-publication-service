7464ba5c0ff7d264b17b8b0f3a518f5fa62bc512
Erika Pauwels <erika.pauwels@gmail.com>
2022-06-13 15:31:46 +0200

    Bump base Docker image

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
 const PUBLIC_GRAPH = 'http://mu.semte.ch/graphs/public';
 const BELGA_PUBLICATION_CHANNEL = 'http://themis.vlaanderen.be/id/publicatiekanaal/04a5d121-c991-493c-b645-b0c67cc53cf6';
 
-
-
 export {
   BELGA_PUBLICATION_CHANNEL,
   PUBLIC_GRAPH
